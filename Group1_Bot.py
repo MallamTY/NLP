f45871cbd86bba1227560285318d6ba96e543dfe
MallamTY <tnsosanya@student.oauife.edu.ng>
2023-06-01 13:36:03 +0100

    Latest commit

--- Group1_Bot.py
+++ Group1_Bot.py
@@ -25,7 +25,7 @@ def indigenious_chatbot(user_input):
     response = None
 
     for term, meaning in yoruba.items():
-        if (user_input.startswith('who is') and term in user_input[7:]) or user_input.startswith('who is a') and term in user_input[9:]:
+        if (user_input.startswith('tani') and term in user_input[5:]) or user_input.startswith('tani a n pe ni') and term in user_input[16:]:
             response = meaning
             break
         
@@ -34,12 +34,12 @@ def indigenious_chatbot(user_input):
             break
 
     if response is None:
-        response = "I'm sorry, I don't have a meaning for the kinship word you supplied or perhaps you haven't supplied one, please try again !!!"
-
+        response = "Mabinu, mi o ni idahun si ibeere ti o bi mi yi, tabi o i ti bi mi ni nkankan, gbiyanju ni enkan si !!!"
+        
     return response
     
 
-domain = input('\nWhich language words do you want to find meaning to >>> ')
+domain = input('\nWhich language will you like to converse with? >>> ')
 
 if domain.lower() == 'english': 
 
@@ -48,7 +48,7 @@ if domain.lower() == 'english':
         if len(user_input) == 0:
             print("\nGroup 1 bot >>> You've asked nothing \n")
 
-        elif user_input.lower() == "exit" or user_input.lower() == "goodbye" or user_input.lower() == "bye" or user_input.lower() == "bye":
+        elif user_input.lower() == "exit" or user_input.lower() == "goodbye" or user_input.lower() == "bye" or user_input.lower() == "bye-bye":
             break
 
         else:
@@ -59,7 +59,7 @@ elif domain.lower() == 'yoruba':
      while True:
         user_input = input('\n Alo Ero >>> ')
         if len(user_input) == 0:
-            print("\nEro Egbe Kini >>> You've asked nothing \n")
+            print("\nEro Egbe Kini >>> o ti bere nkankan \n")
 
         elif user_input.lower() == "jade" or user_input.lower() == "odigba" or user_input.lower() == "odabo":
             break
